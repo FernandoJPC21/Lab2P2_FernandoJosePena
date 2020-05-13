1723693d508c0ff229c18ef8292c07df1ae6ba33
kille <kille@LaptopFernando>
2020-05-12 21:20:25 -0600

    Main del ejercicio 2 de la tarea 2

---
+++ src/Semana1/Ejercicio2Main.java
@@ -0,0 +1,22 @@
+
+package Semana1;
+
+
+import java.util.Scanner;
+
+
+public class Ejercicio2Main {
+    public static void main(String[] args) {
+       
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+}
